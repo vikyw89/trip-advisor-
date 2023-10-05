c216df22e30826483025f0fb332186c38fb33fe0
viky <vikyw89@gmail.com>
2023-10-06 00:42:57 +0800

    fix: refactor scroll styling for better user experience

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -170,7 +170,7 @@ export default function Index() {
           break;
         }
       } catch (err) {
-     
+
       }
     }
 
@@ -380,10 +380,24 @@ export default function Index() {
       direction="column"
       sx={{
         overflowY: "hidden",
-        height: "100%"
+        height: "100%",
       }}
     >
-      <Box sx={{ width: '100%', height: 'inherit', overflowY: "auto" }}>
+      <Box sx={{
+        width: '100%', height: 'inherit', overflowY: "scroll", scrollbarWidth: 'thin',
+        '&::-webkit-scrollbar': {
+          width: '0',
+        },
+        '&::-webkit-scrollbar-track': {
+          background: "#f1f1f1",
+        },
+        '&::-webkit-scrollbar-thumb': {
+          backgroundColor: '#888',
+        },
+        '&::-webkit-scrollbar-thumb:hover': {
+          background: '#555'
+        }
+      }}>
         <Stack
           direction="column"
           justifyContent="space-between"
